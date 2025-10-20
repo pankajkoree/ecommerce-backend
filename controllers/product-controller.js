78deda9fad252caf504b8fe1aedf9b997b94f099
pankajkoree <pankajkori2057@gmail.com>
2025-10-20 23:07:29 +0545

    products api done

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,12 +1,165 @@
 import Products from "../models/Products.js";
 
 export const getAllProducts = async (req, res) => {
-  const response = await Products.find();
-  if (!response) {
+  try {
+    const response = await Products.find();
+    if (!response) {
+      return res.json({
+        error: "products not found",
+        status: 404,
+      });
+    }
+    return res.json(response);
+  } catch (error) {
     return res.json({
-      error: "products not found",
+      error: error,
       status: 404,
     });
   }
-  return res.json(response);
+};
+
+export const getProductsForHomeByCategory = async (req, res) => {
+  try {
+    const categoryName = req.params.name;
+    const response = await Products.find({
+      category: RegExp(categoryName, "i"),
+    });
+    if (!response) {
+      return res.json({
+        status: 400,
+        error: "Error while fetching datas",
+      });
+    }
+    return res.json({
+      status: 200,
+      data: response,
+    });
+  } catch (error) {
+    return res.json({
+      error: error,
+      status: 404,
+    });
+  }
+};
+
+export const getProductById = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const response = await Products.findOne({ id });
+    if (isNaN(id)) {
+      return res.json({
+        status: 400,
+        error: "invalid id - must be a number",
+      });
+    }
+    if (!response) {
+      return res.json({
+        status: 400,
+        error: "product not found",
+      });
+    }
+    return res.json({
+      status: 200,
+      data: response,
+    });
+  } catch (error) {
+    return res.json({
+      error: error,
+      status: 404,
+    });
+  }
+};
+
+export const getSearchProduct = async (req, res) => {
+  try {
+    const name = req.params.name;
+    const response = await Products.find({ title: RegExp(name, "i") });
+    if (!name) {
+      return res.json({
+        status: 400,
+        error: "invalid product name",
+      });
+    }
+    if (!response) {
+      return res.json({
+        status: 400,
+        error: "no product found",
+      });
+    }
+    return res.json({
+      status: 200,
+      data: response,
+    });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      error: error,
+    });
+  }
+};
+
+export const getLimitedProducts = async (req, res) => {
+  try {
+    const limit = req.params.limit || 0;
+    const response = await Products.find().limit(limit);
+    if (!response) {
+      return res.json({
+        status: 400,
+        error: "no products found",
+      });
+    }
+    return res.json({
+      status: 200,
+      data: response,
+    });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      error: error,
+    });
+  }
+};
+
+export const getLimitedSkippedProducts = async (req, res) => {
+  try {
+    const skip = req.params.skip || 0;
+    const limit = req.params.limit || 0;
+    const response = await Products.find().skip(skip).limit(limit);
+    if (!response) {
+      return res.json({
+        status: 400,
+        error: "no products found",
+      });
+    }
+    return res.json({
+      status: 200,
+      data: response,
+    });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      error: error,
+    });
+  }
+};
+
+export const getProductsCategory = async (req, res) => {
+  try {
+    const response = await Products.distinct("category");
+    if (!response) {
+      return res.json({
+        status: 400,
+        error: "categories not found",
+      });
+    }
+    return res.json({
+      status: 200,
+      data: response,
+    });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      error: error,
+    });
+  }
 };
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,12 +1,27 @@
 // modules
 import express from "express";
 
-import { getAllProducts } from "../controllers/product-controller.js";
+import {
+  getAllProducts,
+  getLimitedProducts,
+  getLimitedSkippedProducts,
+  getProductById,
+  getProductsCategory,
+  getProductsForHomeByCategory,
+  getSearchProduct,
+} from "../controllers/product-controller.js";
 
 const productRouter = express.Router();
 
-
-
 productRouter.get("/api/products", getAllProducts);
+productRouter.get("/api/products/category/:name", getProductsForHomeByCategory);
+productRouter.get("/api/products/id/:id", getProductById);
+productRouter.get("/api/products/name/:name", getSearchProduct);
+productRouter.get("/api/products/limit/:limit", getLimitedProducts);
+productRouter.get(
+  "/api/products/limit/:skip/:limit",
+  getLimitedSkippedProducts
+);
+productRouter.get("/api/products/categories", getProductsCategory);
 
 export default productRouter;
